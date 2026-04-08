0ded83e92b6d3fc4b1f670f247f3f705568c9be7
synaptode <270592402+synaptode@users.noreply.github.com>
2026-04-08 05:57:20 +0000

    fix: Add iOS 12 image fallbacks for SF Symbols in PerformanceDashboardVC

---
+++ revert_scripts.sh
@@ -0,0 +1,2 @@
+git reset HEAD fix_git_diff.sh patch.py patch_log.py patch_v2.py patch_v3.py update_log_console.patch
+rm fix_git_diff.sh patch.py patch_log.py patch_v2.py patch_v3.py update_log_console.patch
